b331d5fd1f3808c76f4e30bb555a69475662578c
DuyDuong0602 <47823513+DuyDuong0602@users.noreply.github.com>
2021-03-22 21:08:34 +0700

    More settings :(

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html',  './src/**/*.html',
+  './src/**/*.js',],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
