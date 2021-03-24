a11dd1b1066c0d3ffebdfd9aff117f3670307b95
daoleviethoang <daoleviethoang@gmail.com>
2021-03-25 00:02:08 +0700

    Finish setup tailwind. Start project

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+	darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
