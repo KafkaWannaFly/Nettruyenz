881f7842af0548a04dc9862760a6124af52b6f3b
daoleviethoang <daoleviethoang@gmail.com>
2021-04-16 01:00:38 +0700

    Revert "Revert "Categories page""
    This reverts commit 06a551e998f7733af6a17f8c62ed2ddf4049bb49.

--- Frontend/src/components/Categories/Tag.tsx
+++ Frontend/src/components/Categories/Tag.tsx
@@ -2,22 +2,20 @@ import React from 'react';
 import CloseIcon from '@material-ui/icons/Close';
 import { Console } from 'node:console';
 
-const color = generateColor();
-
-function generateColor() {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    return "#" + randomColor;
-}
 
 export const Tag = (props) => {
+    const color = generateColor();
+
+    function generateColor() {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        return "#" + randomColor;
+}
     console.log(props)
     return (
-        <div id="tags-container">
-            <div className="tag-button" style={{borderColor: color}}>
-                <div className="tag-content">LMAOOOOOOOOOOOOO</div>
-                <div className="remove-tag-icon" style={{color: color}}>
-                    <CloseIcon fontSize="small"/>
-                </div>
+        <div className="tag-button" style={{borderColor: color}}>
+            <div className="tag-content">LMAO</div>
+            <div className="remove-tag-icon" style={{color: color}}>
+                <CloseIcon fontSize="small"/>
             </div>
         </div>
     );
