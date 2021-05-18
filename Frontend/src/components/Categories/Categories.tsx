e533e407d01f2f4b7408cfdd188df10e34fa1efe
DuyDuong0602 <47823513+DuyDuong0602@users.noreply.github.com>
2021-05-18 22:46:07 +0700

    Get and display manga from categories

--- Frontend/src/components/Categories/Categories.tsx
+++ Frontend/src/components/Categories/Categories.tsx
@@ -1,40 +1,81 @@
 
-import { Obj } from "@popperjs/core";
+// import { Obj } from "@popperjs/core";
 import AdvancedSearchBar from "./AdvancedSearchBar";
 import MangaPreviewList from "./MangaPreviewList";
 import TagSection from "./TagSection"
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import {NETTRUYENZ_HOST} from '../../configs'
 import axios from 'axios'
 export function Categories(props) {
 	const [all, setAll] = useState([] as object);
-
+    const [mangas, setMangas] = useState([])
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
     const [input, setInput] = useState([] as string[]);
+
     function getInputFromSearchBar(input : string[]) {
         setInput(input);
+        getMangasAsync();
     }
 
-    useEffect(() => {
-        async function getMangasAsync() {
-            const res = await axios.get(`http://localhost:3000/categories/find-manga`)
-            console.log(res);
-        }
-        getMangasAsync();
-    }, [input])
+    async function getMangasAsync() {
+        const res = await axios.get(`http://localhost:3000/categories`, {params:{tags: tagList, title: input[0], author: input[1], sortBy:input[2], order:input[3], pediod:input[4]}})
+            .then((response) => {
+                setMangas(response.data);
+            })
+            .catch(err => setError(err));
+    }
 
+    // useEffect(() => {
+    //     console.log(input)
+    //     async function getMangasAsync() {
+    //         const res = await axios.get(`http://localhost:3000/categories`, {params:{title: input[0], author: input[1], sortBy:input[2], order:input[3], pediod:input[4]}})
+    //         console.log(res);
+    //     }
+    //     getMangasAsync();
+    // }, [input])
 
 
     const [tagList, setTagList] = useState([] as string[]);
     function getTagListFromTagSection(list : string[]) {
         setTagList(list);
+        getMangasAsync();
     }
+
+
+    // useEffect(() => {
+    //     console.log(tagList);
+    //     async function getMangasAsync() {
+    //         const res = await axios.get(`http://localhost:3000/categories`, {params:{tags: tagList}})
+    //         console.log(res);
+    //     }
+    //     getMangasAsync();
+    // }, [tagList])
+
+    function getRencentlyUpload() {
+        return fetch('http://localhost:3000/recently-uploaded?period=all').then(res => res.json());
+    }
+
+    useEffect(() => {
+        let mounted = true;
+        getRencentlyUpload()
+            .then((result) => {
+                if (mounted) {
+                    setMangas(result);
+                }
+            },
+                (error) => {
+                    setError(error);
+                });
+        setIsLoaded(true);
+    }, []);
 
 	return (
         <>
             <div className="h-full">
                 <AdvancedSearchBar getInputFromSearchBar={getInputFromSearchBar}></AdvancedSearchBar>
                 <TagSection getTagListFromTagSection={getTagListFromTagSection}/>
-                <MangaPreviewList></MangaPreviewList>
+                {isLoaded && <MangaPreviewList mangaList={mangas} />}
             </div>
         </>
     );
--- Frontend/src/components/Categories/MangaPreview.tsx
+++ Frontend/src/components/Categories/MangaPreview.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const MangaPreview: React.FC = () => {
+function MangaPreview(props) {
     return (
-        <figure className="manga-preview-container">
-            <figcaption className="manga-preview-info">
-                <div className="manga-preview-image"></div>
-                <div className="manga-preview-title">Titleeeeeeeeeeeeeeeeeeeeeeeee</div>
-                <div className="flex-container">
-                    <div className="manga-preview-latest-chapter">Chapter</div>
-                    <div className="manga-preview-latest-update">Latest update</div>
-                </div>
-            </figcaption>
-        </figure>
+        <Link to={"/comic/" + props.manga.id}>
+            <figure className="manga-preview-container">
+                <figcaption className="manga-preview-info">
+                    <img className="manga-preview-image" src={props.manga.cover} alt="Sunset in the mountains"></img>
+                    <div>
+                        <div className="manga-preview-title">{props.manga.names[0]}</div>
+                        <div className="flex-container">
+                            <div className="manga-preview-latest-chapter">Chapter</div>
+                            <div className="manga-preview-latest-update">{new Date(props.manga.updatedAt).toLocaleDateString()}</div>
+                        </div>
+                    </div>
+                </figcaption>
+            </figure>
+        </Link>
+
+    //     <>
+    //     <div className="w-full md:w-6/12 lg:w-1/5 lg:mb-0 mb-12 px-2" key={item.id}>
+    //       <div className="max-w-sm px-1 rounded overflow-hidden shadow-lg bg-gray-1200">
+    //         <img className="w-full h-64" src={item.cover} alt="Sunset in the mountains"></img>
+    //         <div className="">
+    //             <div>
+    //                 <Link to={"/comic/" + item.id}>
+    //                     <div className="font-bold text-xl pt-2 truncate">{item.names[0]}</div>
+    //                 </Link>
+    //                 <p className="text-gray-300 text-base truncate">
+    //                     {item.creators}
+    //                 </p>
+    //             </div>
+    //             <div className="pb-2 text-white">
+    //                 <span className="inline-block px-2 text-sm font-semibold  mr-2 mb-2">Chap ...</span>
+    //                 <span className="inline-block px-2 text-sm font-semibold mr-2 mb-2 right">{new Date(item.updatedAt).toLocaleDateString()}</span>
+    //             </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </>
     );
 }
 
